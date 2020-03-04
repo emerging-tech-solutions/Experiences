3638812687ff6598a32ebb55a9537fb7e7e5f7e3
Lwin Maung <lwin@maungs.com>
2020-03-04 14:31:06 -0600

    Token for Speech

--- EmergingExperiences/Conversational/Emgerging.Conversational.UWP/MainPage.xaml.cs
+++ EmergingExperiences/Conversational/Emgerging.Conversational.UWP/MainPage.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +26,54 @@ namespace Emgerging.Conversational.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        MediaCapture mediaCapture;
+        bool isCapturingAudio = false;
+        bool capturingStoppedForAudioState = false;
+        LowLagMediaRecording _mediaRecording;
+
         public MainPage()
         {
             this.InitializeComponent();
+            InitializeMediaElements();
+        }
+
+        private async Task InitializeMediaElements()
+        {
+            mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync();
+            mediaCapture.Failed += MediaCapture_Failed;
+        }
+
+        private void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void btnRecord_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isCapturingAudio)
+            {
+                await InitializeMediaElements();
+                var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                mediaCapture.RecordLimitationExceeded += MediaCapture_RecordLimitationExceeded;
+
+                StorageFile file = await localFolder.CreateFileAsync("audio.mp3", CreationCollisionOption.GenerateUniqueName);
+                _mediaRecording = await mediaCapture.PrepareLowLagRecordToStorageFileAsync(
+                        MediaEncodingProfile.CreateMp3(AudioEncodingQuality.High), file);
+                isCapturingAudio = true;
+                await _mediaRecording.StartAsync();
+            }
+            else
+            {
+                await _mediaRecording.StopAsync();
+                await _mediaRecording.FinishAsync();
+                isCapturingAudio = false;
+            }
+        }
+
+        private void MediaCapture_RecordLimitationExceeded(MediaCapture sender)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ EmergingExperiences/Shared/SharedServices/SpeechServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedServices
+{
+    public class SpeechServices
+    {
+        private string APIKey = "e1d70faa757a4424bae02fc3284b0cbc";
+        private string APIEndPoint = "https://northcentralus.api.cognitive.microsoft.com/sts/v1.0/";
+        private string token;
+
+        public async void Authenticate()
+        {
+            this.token = await FetchTokenAsync("issuetoken");
+        }
+
+        private async Task<string> FetchTokenAsync(string fetchUri)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", APIKey);
+                UriBuilder uriBuilder = new UriBuilder(APIEndPoint + fetchUri);
+
+                var result = await client.PostAsync(uriBuilder.Uri.AbsoluteUri, null);
+                Console.WriteLine("Token Uri: {0}", uriBuilder.Uri.AbsoluteUri);
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
+
+
+    }
+
+
+}
